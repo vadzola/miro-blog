a0f0406600b7308b024a299334cb3c248390e4c9
vadzola <vadzola@yahoo.com.hk>
2017-04-13 02:32:53 +0300

    Sign up form created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :articles
+  get 'signup', to: 'users#new'
 	root 'pages#home'
 	get 'about', to: 'pages#about'
+	resources :users, except: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
