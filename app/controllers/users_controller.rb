b6fbf0a2aa01ac053d57e026a72c1bf97e50b8ac
vadzola <vadzola@yahoo.com.hk>
2017-04-13 11:43:59 +0300

    avatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,6 +29,11 @@ class UsersController < ApplicationController
     
   end
 
+  def show
+  	@user = User.find(params[:id])
+
+  end
+
 	private
 	def user_params
 		params.require(:user).permit(:username, :email, :password)
